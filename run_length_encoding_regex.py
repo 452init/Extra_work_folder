135a99ba05c2013857d8756fd38230a23172b81f
452init <clinsmol10@gmail.com>
2025-11-07 20:00:22 +0300

    Optimized decoding and encoding using regex

--- run_length_encoding_regex.py
+++ run_length_encoding_regex.py
@@ -6,6 +6,14 @@ def decode(string):
     print(''.join(int(count or 1)*char for count, char in matches))
 
 def encode(string):
-    pass
+    char_list = []
+    for match in re.finditer(r'(.)\1*', string): 
+        full_run = match.group(0)
+        
+        if len(full_run) > 1:
+            char_list.append(str(len(full_run)))
+        char_list.append(match.group(1))
+    print(''.join(char_list))
 
 decode('2ab34c4bbca3a2c')
+encode('qqbbbauuattebbbbbcvvv')
